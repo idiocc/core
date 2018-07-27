1003f4de6d33ab52a84a41bb5991e0d882760e6c
Anton <anton@adc.sh>
2018-07-27 22:27:52 +0100

    migrate jsdoc 2 middleware.xml

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import startApp from './lib/start-app'
 
 /**
  * Start the server.
- * @param {MiddlewareConfig} [middleware] Middleware configuration for the `idio` `core` server.
+ * @param {{ session: SessionOptions }} [middleware] Middleware configuration for the `idio` `core` server.
+ * @param {SessionOptions} [middleware.session] Session options.
  * @param {Config} config A configuration object.
  * @param {number} [config.port] Port on which to start the server. Default `5000`.
  * @param {number} [config.host] The host on which to listen. Default `0.0.0.0`.
@@ -14,10 +15,18 @@ async function idioCore(middleware = {}, config = {}) {
   return { url, app, router, middleware: mw }
 }
 
-/* documentary types/index.xml */
+/* documentary types/middleware.xml */
 /**
  * @typedef {import('koa').Context} Context
- * @typedef {(res: import('http').ServerResponse, path: string, stats: import('fs').Stats) => any} SetHeaders
+ *
+ * @typedef {import('fs').Stats} Stats
+ *
+ * @typedef {import('http').ServerResponse} ServerResponse
+ *
+ * @typedef {import('koa-multer').StorageEngine} StorageEngine
+ *
+ * @typedef {(res: ServerResponse, path: string, stats: Stats) => any} SetHeaders
+ *
  * @typedef {Object} StaticConfig
  * @prop {number} maxage=0 Browser cache max-age in milliseconds. Default `0`.
  * @prop {boolean} hidden=false  Allow transfer of hidden files. Default `false`.
@@ -27,20 +36,16 @@ async function idioCore(middleware = {}, config = {}) {
  * @prop {boolean} br=true Try to serve the brotli version of a file automatically when brotli is supported by a client and if the requested file with .br extension exists (note, that brotli is only accepted over https). Default `true`.
  * @prop {SetHeaders} setHeaders Function to set custom headers on response.
  * @prop {boolean} extensions=false Try to match extensions from passed array to search for file when no extension is sufficed in URL. First found is served. Default `false`.
- */
-
-/**
+ *
  * @typedef {Object} SessionConfig
- * @prop {string} [key] cookie key. Default `koa:sess`.
+ * @prop {string} [key="koa:sess"] cookie key. Default `koa:sess`.
  * @prop {number|'session'} [maxAge] maxAge in ms. Default `86400000` (1 day). `session` will result in a cookie that expires when session/browser is closed. Warning: If a session cookie is stolen, this cookie will never expire.
- * @prop {boolean} [overwrite] Can overwrite or not. Default `true`.
- * @prop {boolean} [httpOnly] httpOnly or not or not. Default `true`.
- * @prop {boolean} [true] Signed or not. Default `true`.
- * @prop {boolean} [rolling] Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. Default `false`.
- * @prop {boolean} [renew] Renew session when session is nearly expired, so we can always keep user logged in. Default `false`.
- */
-
-/**
+ * @prop {boolean} [overwrite=true] Can overwrite or not. Default `true`.
+ * @prop {boolean} [httpOnly=true] httpOnly or not or not. Default `true`.
+ * @prop {boolean} [true=true] Signed or not. Default `true`.
+ * @prop {boolean} [rolling=false] Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. Default `false`.
+ * @prop {boolean} [renew=false] Renew session when session is nearly expired, so we can always keep user logged in. Default `false`.
+ *
  * @typedef {Object} Limits
  * @prop {number} [fieldNameSize] Max field name size (Default: 100 bytes).
  * @prop {number} [fieldSize] Max field value size (Default: 1MB).
@@ -55,32 +60,26 @@ async function idioCore(middleware = {}, config = {}) {
  * @prop {StorageEngine} [storage] Where to store the files.
  * @prop {(req: IncomingMessage, file: File, callback: (error: Error | null, acceptFile: boolean)) => void} [fileFilter] Function to control which files are accepted.
  * @prop {Limits} [limits] Limits of the uploaded data.
- * @prop {boolean} [preservePath]  Keep the full path of files instead of just the base name (Default: false).
- */
-
-/**
+ * @prop {boolean} [preservePath=false] Keep the full path of files instead of just the base name. Default `false`.
+ *
  * @typedef {Object} CSRFConfig
- * @prop {string} [invalidSessionSecretMessage]
- * @prop {number} [invalidSessionSecretStatusCode]
- * @prop {string} [invalidTokenMessage]
- * @prop {number} [invalidTokenStatusCode]
- * @prop {string[]} [excludedMethods]
- * @prop {boolean} [disableQuery]
- */
-
-/**
+ * @prop {string} [invalidSessionSecretMessage] 
+ * @prop {number} [invalidSessionSecretStatusCode] 
+ * @prop {string} [invalidTokenMessage] 
+ * @prop {number} [invalidTokenStatusCode] 
+ * @prop {string[]} [excludedMethods] 
+ * @prop {boolean} [disableQuery] 
+ *
  * @typedef {Object} BodyparserConfig
- * @prop {string[]} [enableTypes] Parser will only parse when request type hits enableTypes. Default `['json', 'form']`.
- * @prop {string} [encode] Requested encoding. Default `utf-8` by `co-body`.
- * @prop {string} [formLimit] Limit of the urlencoded body. If the body ends up being larger than this limit a 413 error code is returned. Default `56kb`.
- * @prop {string} [jsonLimit] Limit of the json body. Default `1mb`.
- * @prop {boolean} [strict] When set to true, JSON parser will only accept arrays and objects. Default `true`.
- * @prop {(ctx: Context) => boolean} [detectJSON] Custom json request detect function. Default `null`.
+ * @prop {string[]} [enableTypes="['json', 'form']"] Parser will only parse when request type hits enableTypes. Default `['json', 'form']`.
+ * @prop {string} [encode="utf-8"] Requested encoding. Default `utf-8`.
+ * @prop {string} [formLimit="56kb"] Limit of the urlencoded body. If the body ends up being larger than this limit a 413 error code is returned. Default `56kb`.
+ * @prop {string} [jsonLimit="1mb"] Limit of the json body. Default `1mb`.
+ * @prop {boolean} [strict=true] When set to true, JSON parser will only accept arrays and objects. Default `true`.
+ * @prop {(ctx: Context) => boolean} [detectJSON="null"] Custom json request detect function. Default `null`.
  * @prop {{json: string[], form: string[], text: string[]}} [extendTypes] Support extend types.
  * @prop {(err: Error, ctx: Context) => void} [onerror] Support custom error handle.
- */
-
-/**
+ *
  * @typedef {Object} CompressConfig
  * @prop {(content_type: string) => boolean} [filter] An optional function that checks the response content type to decide whether to compress. By default, it uses `compressible`.
  * @prop {number} [threshold]  Minimum response size in bytes to compress. Default 1024 bytes or 1kb.
@@ -92,25 +91,19 @@ async function idioCore(middleware = {}, config = {}) {
  * @prop {number} [memLevel] compression only
  * @prop {number} [strategy] compression only
  * @prop {any} [dictionary] deflate/inflate only, empty dictionary by default
- */
-
-/**
+ *
  * @typedef {Object} LoggerConfig
  * @prop {(str: string, args: [string, string, string, string, string, string, string]) => void} [transporter] Param `str` is output string with ANSI Color, and you can get pure text with other modules like `strip-ansi`. Param `args` is a array by `[format, method, url, status, time, length]`.
- */
-
-/**
+ *
  * @typedef {Object} Config
- * @prop {number} [port] Port on which to start the server. Default `5000`.
- * @prop {number} [host] The host on which to listen. Default `0.0.0.0`.
- */
-
-/**
+ * @prop {number} [port=5000] Port on which to start the server. Default `5000`.
+ * @prop {string} [host="0.0.0.0"] The host on which to listen. Default `0.0.0.0`.
+ *
  * @typedef {Object} StaticOptions
  * @prop {boolean} [use] Use this middleware for every request.
  * @prop {string|string[]} root Root or multiple roots from which to serve files.
- * @prop {string} [mount] Path from which to serve files. Default `/`.
- * @prop {number} [maxage] How long to cache file for. Default `0`.
+ * @prop {string} [mount="/"] Path from which to serve files. Default `/`.
+ * @prop {number} [maxage=0] How long to cache file for. Default `0`.
  * @prop {StaticConfig} [config] `koa-static` configuration.
  *
  * @typedef {Object} LoggerOptions
